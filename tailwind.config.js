532aef326faf4d71e946a87c4f476bbd196404ed
gizwiz <github.aground374@passmail.net>
2024-02-26 09:03:57 +0100

    more tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //content: ["./views/**/*.templ"], // this is where our templates are located
+  content: ["./views/**/*.templ"], // this is where our templates are located
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
